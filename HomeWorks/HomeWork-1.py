94e7cd5f08fd1b496c1c847aef5438c94ce1fdb3
Kelsen23 <nikolozigoreziani@gmail.com>
2024-08-19 15:52:12 +0400

    first commit

---
+++ HomeWorks/HomeWork-1.py
@@ -0,0 +1,23 @@
+name = 'Nikolozi'
+surname = 'Goreziani'
+
+age = 14
+height = 169.8 
+knows_programming = True
+is_smart = True
+is_ugly = False
+loves_programming = True
+is_chad = True
+
+hobbies = 'Programming, football, swimming, solving problems'
+
+
+print(
+    'My name is: ' + name + ' ' + surname + '. ' +
+    'I am ' + str(age) + ' years old. ' +
+    'My hobbies are: ' + str(hobbies) + '.' +
+    'I am smart: ' + str(is_smart) + ', ' +
+    'I am ugly: ' + str(is_ugly) + ', ' +
+    'I love programming: ' + str(loves_programming) + ', ' +
+    'I am chad: ' + str(is_chad) + '.'
+)
---
+++ Lessons/lesson-1.py
@@ -0,0 +1,14 @@
+name = 'Nikolozi'
+surname = 'Goreziani'
+
+age = 14
+height = 169
+knows_programming = True
+is_smart = True
+is_ugly = False
+
+
+
+
+print(name)
+
